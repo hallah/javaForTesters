ee4e7e40625e62887e4661092506624cd8062f7c
Hugh Hallawell <hhallawell@cainc.com>
2018-02-17 11:51:36 -0500

    Apply formatting conventions.

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -1,16 +1,17 @@
 package com.javafortesters.chap003myfirsttest.examples;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by hugh1 on 11/27/2016.
  */
 public class MyFirstTest {
-    @Test
-    public void  canAddTwoPlusTwo() {
-        int answer = 2+2;
-        assertEquals("2+2=4", 4, answer);
+	@Test
+	public void canAddTwoPlusTwo() {
+		int answer = 2 + 2;
+		assertEquals("2+2=4", 4, answer);
 
-    }
+	}
 }
