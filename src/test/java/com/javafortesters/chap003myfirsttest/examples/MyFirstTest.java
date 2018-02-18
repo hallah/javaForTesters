01719a38b57b1f24b426e3e56958683a5ef2b234
Hugh Hallawell <hhallawell@cainc.com>
2018-02-18 10:26:37 -0500

    Add another negative test case.

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -3,6 +3,7 @@ package com.javafortesters.chap003myfirsttest.examples;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by hugh1 on 11/27/2016.
@@ -12,5 +13,11 @@ public class MyFirstTest {
 	public void canAddTwoPlusTwo() {
 		int answer = 2 + 2;
 		assertEquals("2+2=4", 4, answer);
+	}
+
+	@Test
+	public void negativeTwoPlusTwoNot5() {
+		int answer = 2 + 2;
+		assertNotEquals("2+2!=4", 5, answer);
 	}
 }
