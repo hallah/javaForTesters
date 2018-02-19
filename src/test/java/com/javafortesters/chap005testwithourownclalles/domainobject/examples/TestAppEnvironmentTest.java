3ed6152e7fb6cf53a34777c789ddc2997addf765
Hugh Hallawell <hhallawell@cainc.com>
2018-02-19 07:27:48 -0500

    Minor assert update.

--- src/test/java/com/javafortesters/chap005testwithourownclalles/domainobject/examples/TestAppEnvironmentTest.java
+++ src/test/java/com/javafortesters/chap005testwithourownclalles/domainobject/examples/TestAppEnvironmentTest.java
@@ -1,11 +1,12 @@
 package com.javafortesters.chap005testwithourownclalles.domainobject.examples;
 
 import com.javafortesters.domainobject.TestAppEnv;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestAppEnvironmentTest {
 	@Test
 	public void canGetUrlStatically() {
-		TestAppEnv.getUrl();
+		Assert.assertEquals("Environment url should equal http://192.123.0.3:67", "http://192.123.0.3:67", TestAppEnv.getUrl());
 	}
 }
