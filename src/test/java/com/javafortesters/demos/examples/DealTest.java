76119c71731983cbafdd23258f301bb10b1a503c
Hugh Hallawell <hhallawell@cainc.com>
2018-03-13 17:11:02 -0400

    Refactoring practice.

--- src/test/java/com/javafortesters/demos/examples/DealTest.java
+++ src/test/java/com/javafortesters/demos/examples/DealTest.java
@@ -3,7 +3,10 @@ package com.javafortesters.demos.examples;
 import com.javafortesters.business.Card;
 import com.javafortesters.business.Dealer;
 import com.javafortesters.business.Deck;
+import com.javafortesters.business.Player;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -12,17 +15,24 @@ public class DealTest {
 	@Test
 	public void testDealingOfCards() {
 		Deck deck = new Deck();
-		Dealer.deal(deck.getDeck(), deck.getPlayers());
+		List<Player> players = deck.getPlayers();
+		Dealer.deal(deck.getDeck(), players);
 
-		for (int i = 0; i < deck.getPlayers().size(); i++) {
-			assertThat(deck.getPlayers().get(i).getCardsInHand().size(), is(5));
+		for (int i = 0; i < players.size(); i++) {
+			List<Card> cardsInHand = players.get(i).getCardsInHand();
+			assertThat(cardsInHand.size(), is(5));
 
-			System.out.println(deck.getPlayers().get(i).getName() + " has the following cards:");
-			for (Card aCard : deck.getPlayers().get(i).getCardsInHand()) {
-				System.out.println(aCard.toString());
-			}
-			System.out.println();
+			outputCardDetailsToConsole(players, i, cardsInHand, deck);
 		}
 		assertThat(deck.getDeck().size(), is(32));
+	}
+
+	private void outputCardDetailsToConsole(List<Player> players, int playerNumber, List<Card> cardsInHand, Deck deck) {
+		System.out.println(players.get(playerNumber).getName() + " has the following cards:");
+		for (Card aCard : cardsInHand) {
+			System.out.println(aCard.toString());
+		}
+		System.out.println("The deck has " + deck.getDeck().size() + " cards in it.");
+		System.out.println();
 	}
 }
