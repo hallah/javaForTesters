f4081617d5d871d028377f39a4dc1229772bd503
Hugh Hallawell <hallah@gmx.com>
2018-03-13 15:31:56 -0400

    Adding first test and method.

--- src/main/java/com/javafortesters/demos/examples/MyCustomSet.java
+++ src/main/java/com/javafortesters/demos/examples/MyCustomSet.java
@@ -10,7 +10,23 @@ public class MyCustomSet {
 	}
 
 	public MyCustomSet(Collection<Object> c) {
-		array = c.toArray(new Object[c.size()]);
+		array = new Object[0];
+		for (Object o : c) {
+			boolean exists = false;
+			for (Object i : array) {
+				if (o.equals(i)) {
+					exists = true;
+				}
+			}
+			if (!exists) {
+				Object arrayCopy[] = array.clone();
+				array = new Object[arrayCopy.length + 1];
+				for (int j = 0; j < arrayCopy.length; j++) {
+					array[j] = arrayCopy[j];
+				}
+				array[arrayCopy.length] = o;
+			}
+		}
 	}
 
 	public Object[] toArray() {
